e4e9f17c11d681f7356b159f531fe42380a350ce
Sagar Yeole <info@voracious.in>
2020-01-17 23:06:18 +0530

    User creation done

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+use App\Role;
+use App\Http\Requests\UserRequest;
 use Illuminate\Http\Request;
 
 class AdminUsersController extends Controller
@@ -14,7 +17,10 @@ class AdminUsersController extends Controller
     public function index()
     {
         //
-        return view('admin.users.index');
+
+        $users = User::all();
+
+        return view('admin.users.index',compact('users'));
     }
 
     /**
@@ -24,7 +30,8 @@ class AdminUsersController extends Controller
      */
     public function create()
     {
-        //
+        $roles = Role::pluck('name','id')->all();
+        return view('admin.users.create',compact('roles'));
     }
 
     /**
@@ -33,9 +40,12 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UserRequest $request)
     {
-        //
+        User::create($request->all());
+
+        return redirect('/admin/users');
+        // return $request->all();
     }
 
     /**
