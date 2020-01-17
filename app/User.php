e1ea862addcabaa48b1fa333de0ab15e99d06a68
Sagar Yeole <info@voracious.in>
2020-01-17 20:17:10 +0530

    Admin page setup

--- app/User.php
+++ app/User.php
@@ -28,6 +28,11 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+
+    public function role(){
+        return $this->belongsTo('App\Role');
+    }
+
     /**
      * The attributes that should be cast to native types.
      *
